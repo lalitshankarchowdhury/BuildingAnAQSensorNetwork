d75fd6bcf0e99f4ec289071dedc8d8ccbd05d7f2
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2022-04-05 15:18:50 +0530

    Commit changes

--- sps30/write_sps30_data.py
+++ sps30/write_sps30_data.py
@@ -3,6 +3,7 @@
 
 import sps30
 import time
+import csv
 
 # Connect to sensor on I2C address 0x69
 sensor = sps30.SPS30(1)
@@ -44,22 +45,46 @@ with open(csv_file_name, "w", encoding="utf-8") as csvfile:
         csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
     )
 
+    # Write CSV dataset header row
+    csv_writer.writerow(
+        [
+            "PM1.0 in µg/m3",
+            "PM2.5 in µg/m3",
+            "PM4.0 in µg/m3",
+            "PM10.0 in µg/m3",
+            "NC1.0 in particles/cm3",
+            "NC2.5 in particles/cm3",
+            "NC4.0 in particles/cm3",
+            "NC10.0 in particles/cm3",
+            "Typical Particle Size in µm",
+        ]
+    )
+
+    running_time = int(input("Enter number of readings to take (Time = ~1s/reading): "))
+    cnt = 0
+
     # Run until keyboard interrupt
     try:
-        while True:
+        while cnt < running_time:
             # Read measured values
             try:
                 sensor.read_measured_values()
 
-                print(f"PM1.0: {sensor.dict_values['pm1p0']} µg/m3: ")
-                print(f"PM2.5: {sensor.dict_values['pm2p5']} µg/m3: ")
-                print(f"PM4.0: {sensor.dict_values['pm4p0']} µg/m3: ")
-                print(f"PM10.0: {sensor.dict_values['pm10p0']} µg/m3: ")
-                print(f"NC1.0: {sensor.dict_values['nc1p0']}/cm3: ")
-                print(f"NC2.5: {sensor.dict_values['nc2p5']}/cm3: ")
-                print(f"NC4.0: {sensor.dict_values['nc4p0']}/cm3: ")
-                print(f"NC10.0: {sensor.dict_values['nc10p0']}/cm3: ")
-                print(f"Typical Particle Size: {sensor.dict_values['typical']} µm")
+                csv_writer.writerow(
+                    [
+                        sensor.dict_values["pm1p0"],
+                        sensor.dict_values["pm2p5"],
+                        sensor.dict_values["pm4p0"],
+                        sensor.dict_values["pm10p0"],
+                        sensor.dict_values["nc1p0"],
+                        sensor.dict_values["nc2p5"],
+                        sensor.dict_values["nc4p0"],
+                        sensor.dict_values["nc10p0"],
+                        sensor.dict_values["typical"],
+                    ]
+                )
+
+                cnt += 1
             except:
                 pass
 
