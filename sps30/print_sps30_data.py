23f66b832cdff9121718471b492c4e2a2209c506
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2022-04-05 14:12:59 +0530

    Add files

---
+++ sps30/print_sps30_data.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sps30
+import time
+
+# Connect to sensor on I2C address 0x69
+sensor = sps30.SPS30(1)
+
+# # Set auto-cleaning interval to default value
+# sensor.set_auto_cleaning_interval(604800)
+
+# # Reset sensor to check new auto-cleaning interval
+# sensor.device_reset()
+
+# Start measuring data
+sps.start_measurement()
+
+while True:
+    print("PM1.0 Value in µg/m3: " + str(sps.dict_values["pm1p0"]))
+    print("PM2.5 Value in µg/m3: " + str(sps.dict_values["pm2p5"]))
+    print("PM4.0 Value in µg/m3: " + str(sps.dict_values["pm4p0"]))
+    print("PM10.0 Value in µg/m3: " + str(sps.dict_values["pm10p0"]))
+    print("NC0.5 Value in 1/cm3: " + str(sps.dict_values["nc0p5"]))
+    print("NC1.0 Value in 1/cm3: " + str(sps.dict_values["nc1p0"]))
+    print("NC2.5 Value in 1/cm3: " + str(sps.dict_values["nc2p5"]))
+    print("NC4.0 Value in 1/cm3: " + str(sps.dict_values["nc4p0"]))
+    print("NC10.0 Value in 1/cm3: " + str(sps.dict_values["nc10p0"]))
+    print("Typical Particle Size in µm: " + str(sps.dict_values["typical"]))
