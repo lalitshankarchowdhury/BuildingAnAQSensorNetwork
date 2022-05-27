c0c8d5e657122135f5c48085a25aa557b5a67701
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2022-05-27 15:49:38 +0530

    Update files

--- setup.sh
+++ setup.sh
@@ -8,5 +8,7 @@ source .venv/bin/activate
 
 echo 'Installing dependencies'
 git clone https://github.com/lalitshankarchowdhury/bme68x-python-library
-.venv/bin/python3 bme68x-python-library/setup.py
+cd bme68x-python-library/
+../.venv/bin/python3 setup.py install
+cd ..
 pip install -r requirements.txt
