082a7d25659632ad5eefa99d2ebfcfbfc78c3cf2
Samkit Kundalia <samkitkundalia@gmail.com>
2022-06-03 16:44:08 +0530

    Rename files

---
+++ bme680/db-test.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+conn = sqlite3.connect("bme680.db")
+c = conn.cursor()
+
+for row in c.execute("""SELECT * FROM bme680"""):
+    print(row)
+
+conn.close()
+
+
+# conn = sqlite3.connect("db-rp1")
+# c = conn.cursor()
+
+# # Query for Inserting
+# c.execute(
+#     """
+#           INSERT INTO bme680 (sample_nr, timestamp, raw_temperature, raw_pressure, raw_humidity, raw_gas, status)
+#             VALUES
+#                 (1,'12:58 PM',29.807947158813477,993.4461059570312,37.93738555908203,2397.1376953125,160),
+#                 (2,'2:55 AM',50.807947158813477,80.4461059570312,32.93738555908203,2346.1376953125,180)"""
+# )
+
+# conn.commit()
---
+++ sps30/db-test.py
@@ -0,0 +1,25 @@
+#! ../.venv/bin/python3
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+conn = sqlite3.connect("sps30.db")
+c = conn.cursor()
+
+for row in c.execute("""SELECT * FROM sps30"""):
+    print(row)
+
+conn.close()
+
+# conn = sqlite3.connect("sps30.db")
+# c = conn.cursor()
+
+# # Query for inserting
+# c.execute(
+#     """INSERT INTO sps30 (pm1p0, pm2p5, pm4p0, pm10p0, nc0p5, nc1p0, nc2p5, nc4p0, nc10p0, typical)
+#             VALUES
+#                 (9.40277099609375, 12.014436721801758, 13.747060775756836, 14.659709930419922, 58.250694274902344, 71.34091186523438, 73.86448669433594, 74.30712890625, 74.41680145263672, 0.6754058599472046),
+#                 (5.255046367645264, 10.539371490478516, 14.584619522094727, 16.71546173095703, 22.73638153076172, 35.775390625, 41.28406524658203, 42.282203674316406, 42.509342193603516, 0.7761850357055664) """
+# )
+
+# conn.commit()
