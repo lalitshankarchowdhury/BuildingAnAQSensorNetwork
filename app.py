7f8f9c78af4a2d01d35d099d7c3bdabfc156047f
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2022-06-06 12:44:02 +0530

    Update: 2022-06-06 @ 12:44:02

--- app.py
+++ app.py
@@ -9,10 +9,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    conn = sqlite3.connect("./sps30/sps30.db")
-    c = conn.cursor()
-    row = c.execute(
+    sps30_conn = sqlite3.connect("./sps30/sps30.db")
+    sps30_curr = sps30_conn.cursor()
+
+    bme680_conn = sqlite3.connect("./bme680/bme680.db")
+    bme680_curr = bme680_conn.cursor()
+
+    sps30_row = sps30_curr.execute(
         """SELECT * FROM sps30 ORDER BY actual_time DESC LIMIT 1"""
+    ).fetchone()
+
+    bme680_row = bme680_curr.execute(
+        """SELECT * FROM bme680 ORDER BY actual_time DESC LIMIT 1"""
     ).fetchone()
 
     html = f"""
@@ -22,7 +30,15 @@ def index():
                     <title>
                         Realtime sensor reading
                     </title>
-
+                    <style>
+                    * {{
+                    font-family: monospace;
+                    }}
+                    table,th,td {{
+                    border-width: 2px;
+                    border-style: solid;
+                    }}
+                    </style>
                     <script>
                         function autoRefresh() {{
                                 window.location = window.location.href;
@@ -31,7 +47,8 @@ def index():
                     </script>
                 </head>
                 <body>
-                    <h1>Sensor output!</h1>
+                    <h1>Sensor readings</h1>
+                    <h2>Sensirion SPS30</h2>
                     <table>
                         <tr>
                             <th>Serial number</th>
@@ -48,18 +65,18 @@ def index():
                             <th>typical</th>
                         </tr>
                         <tr>
-                            <td>{row[0]}</td>
-                            <td>{row[1]}</td>
-                            <td>{row[2]}</td>
-                            <td>{row[3]}</td>
-                            <td>{row[4]}</td>
-                            <td>{row[5]}</td>
-                            <td>{row[6]}</td>
-                            <td>{row[7]}</td>
-                            <td>{row[8]}</td>
-                            <td>{row[9]}</td>
-                            <td>{row[10]}</td>
-                            <td>{row[11]}</td>
+                            <td>{sps30_row[0]}</td>
+                            <td>{sps30_row[1]}</td>
+                            <td>{sps30_row[2]}</td>
+                            <td>{sps30_row[3]}</td>
+                            <td>{sps30_row[4]}</td>
+                            <td>{sps30_row[5]}</td>
+                            <td>{sps30_row[6]}</td>
+                            <td>{sps30_row[7]}</td>
+                            <td>{sps30_row[8]}</td>
+                            <td>{sps30_row[9]}</td>
+                            <td>{sps30_row[10]}</td>
+                            <td>{sps30_row[11]}</td>
                         </tr>
                     </table>
                 </body>
